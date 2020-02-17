f8224199da80528b8d15d9da7a72b2ac4b9593ba
Alexander Rubenov <alexander.rubenov@gmail.com>
2020-02-17 23:47:58 +0300

    added sass

---
+++ src/components/app/app.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './app.module.scss'
+
+const App = () => {
+  return <div className={styles.test}>fd</div>
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // components
-import App from './app'
+import App from './components/app'
 
 ReactDOM.render(<App />, document.getElementById('root'))
