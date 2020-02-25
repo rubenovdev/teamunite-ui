ce795a0669c01f4d9eeb67b03c9b141275615055
Alexander Rubenov <alexander.rubenov@gmail.com>
2020-02-26 01:35:04 +0300

    added eslint

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import styles from './app.module.scss'
 import Header from '../header'
 import { BrowserRouter } from 'react-router-dom'
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Header />
