6bcb8ddec256387d25d1e094e36701713d4ec368
Vadim Shkolenko <vadimand1998@gmail.com>
2020-03-05 18:44:01 +0300

    added wrapper

---
+++ src/components/wrapper/wrapper.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './wrapper.module.scss'
+
+const Wrapper: React.FC = () => {
+  return <div className={styles.wrapper}></div>
+}
+
+export default Wrapper
