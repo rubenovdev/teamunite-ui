b7825ff78a760e14cd8c809778a3db69b4f9bb90
MrMargaryan <hamlet.java@gmail.com>
2020-03-15 20:54:10 +0300

    changed styles

--- src/components/close-button/close-button.tsx
+++ src/components/close-button/close-button.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import boldCloseButton from '../../assests/svgs/bold-close-button.svg'
-import lightCloseButton from '../../assests/svgs/light-close-button.svg'
+import boldCloseButton from '../../assets/images/bold-close-button.svg'
+import lightCloseButton from '../../assets/images/light-close-button.svg'
 
 const CloseButton: React.FC = () => {
   const [width, setWidth] = useState<number | null>(null)
@@ -8,15 +8,24 @@ const CloseButton: React.FC = () => {
   useEffect(() => {
     !width && setWidth(window.innerWidth)
 
-    window.addEventListener("resize", () => setWidth(window.innerWidth))
+    window.addEventListener('resize', () => setWidth(window.innerWidth))
 
-    return () => window.removeEventListener("resize", () => setWidth(window.innerWidth))
+    return (): void =>
+      window.removeEventListener('resize', () => setWidth(window.innerWidth))
   })
 
-  return (
-    width && width < 768
-      ? <img style={{ display: 'block' }} src={lightCloseButton} alt="Закрыть модальное окно" />
-      : <img style={{ display: 'block' }} src={boldCloseButton} alt="Закрыть модальное окно" />
+  return width && width < 768 ? (
+    <img
+      style={{ display: 'block' }}
+      src={lightCloseButton}
+      alt="Закрыть модальное окно"
+    />
+  ) : (
+    <img
+      style={{ display: 'block' }}
+      src={boldCloseButton}
+      alt="Закрыть модальное окно"
+    />
   )
 }
 
