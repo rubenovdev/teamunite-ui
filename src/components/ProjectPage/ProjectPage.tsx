9b26bbca9c4d72fbdf1248cad66de7d5b798498d
MrMargaryan <hamlet.java@gmail.com>
2020-03-21 19:56:55 +0300

    created ProjectPage component

---
+++ src/components/ProjectPage/ProjectPage.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react'
+
+// import styles from './ProjectPage.module.scss'
+
+const ProjectPage: FC = () => {
+  return (
+    <div>
+      <h1>Hello Page</h1>
+    </div>
+  )
+}
+
+export default ProjectPage
