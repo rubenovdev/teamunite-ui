71f90a6353a0e14a1ff10fb9f77d042e93a82a07
Vadim Shkolenko <vadimand1998@gmail.com>
2020-04-20 03:33:40 +0300

    added types for arrayToMap

--- src/store/utils.ts
+++ src/store/utils.ts
@@ -1,5 +1,7 @@
-export const arrayToMap = (arr: any) =>
-  arr.reduce((map: any, item: any) => {
+export const arrayToMap = <T extends { _id: string }>(
+  arr: Array<T>
+): { [key: string]: T } =>
+  arr.reduce((map: { [key: string]: T }, item: T) => {
     return {
       ...map,
       [item._id]: item,
