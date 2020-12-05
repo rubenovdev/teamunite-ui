13f1e0d3633126b10d01fdb1d0730023405021d9
MrMargaryan <hamlet.java@gmail.com>
2020-12-05 03:40:22 +0300

    created sidemenu component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Wrapper from './components/Wrapper/Wrapper'
 import Header from './components/Header/Header'
+import SideMenu from './components/SideMenu/SideMenu'
 import Assets from './pages/Assets/Assets'
 
 const App: FC = () => {
@@ -9,6 +10,7 @@ const App: FC = () => {
     <Router>
       <Wrapper>
         <Header />
+        <SideMenu />
         <Switch>
           <Route path="/assets" component={Assets} />
         </Switch>
