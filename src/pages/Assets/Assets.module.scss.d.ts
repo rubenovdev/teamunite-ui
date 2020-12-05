3f5f335842e4595cad8d51fa97e73ce4e59e34d3
MrMargaryan <hamlet.java@gmail.com>
2020-12-05 17:55:41 +0300

    created button components

--- src/pages/Assets/Assets.module.scss.d.ts
+++ src/pages/Assets/Assets.module.scss.d.ts
@@ -1,2 +1,4 @@
 export const container: string;
 export const inputGroup: string;
+export const buttons: string;
+export const button: string;
--- src/pages/Assets/Assets.tsx
+++ src/pages/Assets/Assets.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from 'react'
-import Input from '../../components/Input/Input'
 import useInput from '../../hooks/useInput'
+import Input from '../../components/Input/Input'
+import Button from '../../components/Button/Button'
+import AddButton from '../../components/AddButton/AddButton'
+import DeleteButton from '../../components/DeleteButton/DeleteButton'
 
 import styles from './Assets.module.scss'
 
@@ -35,7 +38,57 @@ const Assets: FC = () => {
         value={test3.value}
         onChange={test3.onChange}
         error="Поле не должно быть пустым"
+        inputGroupClassName={styles.inputGroup}
       />
+      <div className={styles.buttons}>
+        <Button
+          type="button"
+          style="primary"
+          text="Primary"
+          onClick={() => console.log('primary')}
+          className={styles.button}
+        />
+        <Button
+          type="button"
+          isDisabled={true}
+          style="primary"
+          text="Disabled"
+          onClick={() => console.log('disabled')}
+          className={styles.button}
+        />
+        <Button
+          type="button"
+          style="secondary"
+          text="Secondary"
+          onClick={() => console.log('secondary')}
+          className={styles.button}
+        />
+        <Button
+          type="button"
+          style="delete"
+          text="Delete"
+          onClick={() => console.log('delete')}
+          className={styles.button}
+        />
+        <Button
+          type="button"
+          style="primary"
+          text="Large primary"
+          onClick={() => console.log('large primary')}
+          isLarge={true}
+          className={styles.button}
+        />
+        <Button
+          type="button"
+          style="delete"
+          text="Large delete"
+          onClick={() => console.log('large delete')}
+          isLarge={true}
+          className={styles.button}
+        />
+        <AddButton type="button" onClick={() => console.log('add button')} className={styles.button} />
+        <DeleteButton type="button" onClick={() => console.log('delete button')} className={styles.button} />
+      </div>
     </div>
   )
 }
