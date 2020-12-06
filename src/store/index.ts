056f97af888cf828d1baadca8fd8155d11e0c959
Vadim Shkolenko <vadimand1998@gmail.com>
2020-12-06 19:54:20 +0300

    added store

---
+++ src/store/index.ts
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+
+import { rootReducer } from './reducers'
+
+const enhancer = applyMiddleware(thunk, logger)
+
+const store = createStore(rootReducer, enhancer)
+
+export default store
